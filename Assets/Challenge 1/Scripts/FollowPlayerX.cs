6599bbdb698ecd748470fceb72a0eea83a7bec41
Steven Kennedy <umbrylax@gmail.com>
2022-02-05 17:30:35 -0700

    fixed offest camera

--- Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
+    private Vector3 offset = new Vector3(25, 0, 1);
 
     // Start is called before the first frame update
     void Start()
